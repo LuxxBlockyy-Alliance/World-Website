74f5c1df545dae1e3fc739e88561b37553d60dc8
LuxxDevs <lkgames256@gmail.com>
2024-01-06 23:56:26 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='website')
 
+
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True, host='172.17.0.3', port=8080)
